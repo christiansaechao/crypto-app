e936c85a216f039757c00ca74d01b116ef72601d
christiansaechao <christiansaechao23@gmail.com>
2022-11-16 19:48:08 -0800

    small changes to variables

--- src/components/LandingPageCharts/LandingPageCharts.js
+++ src/components/LandingPageCharts/LandingPageCharts.js
@@ -15,18 +15,18 @@ ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, PointEleme
 export class LandingPageCharts extends Component {
   render() {
     const { chartsData } = this.props;
-    console.log(chartsData); 
+    
     return (
       <>
         {chartsData && (
           <Bar
             data={{
-              labels: this.props.chartsData.prices.map((date) => {
+              labels: chartsData.prices.map((date) => {
                 return new Date(date[0]).getDate();
               }),
               datasets: [
                 {
-                  data: this.props.chartsData.prices.map((price) => {
+                  data: chartsData.prices.map((price) => {
                     return parseInt(price[1].toFixed(2));
                   }),
                   backgroundColor: "red",
@@ -39,12 +39,12 @@ export class LandingPageCharts extends Component {
         {chartsData && (
           <Line
             data={{
-              labels: this.props.chartsData.total_volumes.map((date) => {
+              labels: chartsData.total_volumes.map((date) => {
                 return new Date(date[0]).getDate();
               }),
               datasets: [
                 {
-                  data: this.props.chartsData.total_volumes.map((volume) => {
+                  data: chartsData.total_volumes.map((volume) => {
                     return parseInt(volume[1].toFixed(2));
                   }),
                   backgroundColor: "red",
