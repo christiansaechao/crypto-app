a239ea0770810d60eae070c1b1aa353328dcab8f
christiansaechao <christiansaechao23@gmail.com>
2022-11-22 21:30:10 -0800

    added infinite scroll to table

--- src/Pages/CoinsPage/CoinsPage.js
+++ src/Pages/CoinsPage/CoinsPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { CoinsContainer } from "./CoinsPage.styles";
 import CoinsTable from "../../components/CoinsTable/CoinsTable";
 import LandingPageCharts from "../../components/LandingPageCharts/LandingPageCharts";
@@ -8,21 +9,23 @@ const URL = "https://api.coingecko.com/api/v3/coins/";
 export default class CoinsPage extends Component {
   state = {
     coinsData: [],
-    coinsPerPage: 5,
+    coinsPerPage: 20,
     pageNum: 1,
     chartsData: null,
     numDays: 30,
+    hasMoreItems: true,
   };
 
   getCoinsData = async () => {
-      try { 
-        const { data } = await axios(
-          `${URL}markets?vs_currency=${this.props.selectedCurrency}&order=market_cap_desc&per_page=${this.state.coinsPerPage}&page=${this.state.pageNum}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
-        );
-        this.setState({ coinsData: data })
-      } catch (err) {
-        console.log(err.error);
-      }
+    try {
+      const { data } = await axios(
+        `${URL}markets?vs_currency=${this.props.selectedCurrency}&order=market_cap_desc&per_page=${this.state.coinsPerPage}&page=${this.state.pageNum}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+      );
+      this.setState({ coinsData: [...this.state.coinsData, ...data] });
+      console.log(this.state);
+    } catch (err) {
+      console.log(err.error);
+    }
   };
 
   getChartsData = async () => {
@@ -39,13 +42,17 @@ export default class CoinsPage extends Component {
   };
 
   updatePageNum = () => {
-    this.setState({pageNum: this.state.pageNum + 1})
-  }
+    this.setState({ pageNum: this.state.pageNum + 1 });
+  };
 
   componentDidUpdate(prevProps, prevState) {
     if (this.props.selectedCurrency !== prevProps.selectedCurrency) {
       this.getCoinsData();
       this.getChartsData();
+    }
+
+    if(this.state.pageNum !== prevState.pageNum){
+      this.getCoinsData();
     }
   }
 
@@ -55,13 +62,20 @@ export default class CoinsPage extends Component {
   }
 
   render() {
-    const { coinsData, chartsData } = this.state;
+    const { coinsData, chartsData, hasMoreItems } = this.state;
     return (
       <>
         {coinsData && (
           <CoinsContainer>
             <LandingPageCharts chartsData={chartsData} />
-            <CoinsTable coinsData={coinsData} />
+            <InfiniteScroll
+              dataLength={coinsData.length}
+              next={this.updatePageNum}
+              hasMore={hasMoreItems}
+              loader={<h4>Loading...</h4>}
+            >
+              <CoinsTable coinsData={coinsData} />
+            </InfiniteScroll>
           </CoinsContainer>
         )}
       </>
