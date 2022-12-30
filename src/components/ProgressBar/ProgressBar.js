0beea967ebdab82b2ab20b1d39c082e5b8761169
christiansaechao <christiansaechao23@gmail.com>
2022-12-29 21:41:24 -0800

    added filters to coins table

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -4,7 +4,7 @@ import { Progress, Container } from "./ProgressBar.styles";
 const ProgressBar = ({coin}) => {
   return (
     <Container width={100}>
-        <Progress percent={(coin.circulating_supply / coin.total_supply) * 100} />
+        <Progress percent={coin.total_supply > 0 ? (coin.circulating_supply / coin.total_supply) * 100 : (coin.circulating_supply) * 100} />
     </Container>
   )
 }
